aca356e1a208a3c67c2c6f24748b20c9ad8ffd1a
Guilherme <guiclopes1@gmail.com>
2026-08-17 14:47:45 -0300

    Revisão de variáveis

---
+++ 03_revisao.js
@@ -0,0 +1,28 @@
+/*
+let aluguel = 1200.00
+let condominio = 250.59
+let internet = 85.99
+let soma = aluguel + condominio + internet
+
+console.log(" O total das contas é :" +soma)
+
+//-------------------------------------------------------------------------------------
+
+let nome = "Guilherme"
+let sobrenome = "Carvalho"
+let nome_completo = nome + " " + sobrenome
+console.log("Seja bem-vindo (a) " +nome_completo)
+*/
+//-------------------------------------------------------------------------------------
+
+let salario = 2000
+console.log("Seu salario é R$ " +salario)
+
+let comissao = 100
+salario = salario + comissao 
+console.log("Salário com comissão " +salario)
+
+let adicional_noturno = 200
+salario = salario + adicional_noturno
+console.log("Salário com adicional noturno " +salario)
+
